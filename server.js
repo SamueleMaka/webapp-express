e14f04acc8accbd2f32af9f7417f3ae2a2a12e21
SamueleMaka <samuele.maka.imt@gmail.com>
2025-08-05 19:45:11 +0200

    feat: cors

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express()
 const port = 3000
 const router = require("./router/router")
-
+const cors = require("cors")
 app.use("/api/movies", router)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
+app.use(cors())
