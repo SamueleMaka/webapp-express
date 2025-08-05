fe2743849e3d71ab47e3c349af1a812a37d13844
SamueleMaka <samuele.maka.imt@gmail.com>
2025-08-05 20:02:41 +0200

    fix: fixed cors

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const app = express()
 const port = 3000
 const router = require("./router/router")
 const cors = require("cors")
+
+app.use(cors());
+app.use(express.json());
+
 app.use("/api/movies", router)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
-app.use(cors())
