32936e4ebffd1ba93324b7361198000c68528f26
SamueleMaka <samuele.maka.imt@gmail.com>
2025-08-06 14:54:55 +0200

    feat: show reviews

--- controller/controller.js
+++ controller/controller.js
@@ -1,6 +1,6 @@
 const connection = require("../data/connection")
 
-function index (req,res) {
+function index(req,res) {
     const sql = "SELECT * FROM movies.movies";
 
     connection.query(sql, (error, results)=>{
@@ -13,19 +13,36 @@ function index (req,res) {
 
 function show(req, res) {
     const currMovie = req.params.id;
-    const sql = 'SELECT * FROM movies.movies WHERE id = ?';
-  
-    connection.query(sql, [currMovie], (error, results) => {
+    const sqlMovie = 'SELECT * FROM movies.movies WHERE id = ?';
+    const sqlReview = 'SELECT * FROM movies.reviews WHERE id = ?';
+    let movieDetails
+    connection.query(sqlMovie, [currMovie], (error, movieResults) => {
       if (error) {
         return res.status(500).send("errore nel server")
       }
-  
-      if (results.length === 0) {
+      if (movieResults.length === 0) {
         return res.status(404).send("film non trovato")
       }
+      movieDetails = movieResults[0]
+
+      connection.query(sqlReview, [currMovie], (error, revResults) =>{
+        if(error) {
+          return res.status(500).send("errore nel server")
+        }
   
-      res.json(results[0]);
+        const movieReview = {
+          id: movieDetails.id,
+          title: movieDetails.title,
+          director: movieDetails.director,
+          image: movieDetails.image,
+          abstract: movieDetails.abstract,
+          reviews: revResults
+        }
+        res.json(movieReview)
+      })
     });
+
+    
 }
 
 module.exports = {index, show}
